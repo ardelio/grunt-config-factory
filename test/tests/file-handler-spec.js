8219f74c783db59c596b245ee1c0dde49b40bea7
Anthony Sceresini <asceresini@gmail.com>
2015-01-31 22:08:03 +1100

    can retrieve key from filename

--- test/tests/file-handler-spec.js
+++ test/tests/file-handler-spec.js
@@ -11,3 +11,15 @@ exports.testIsNotValid = function(test) {
   test.strictEqual(fileHandler('file.txt').isValid(), false);
   test.done();
 };
+
+exports.testRetrieveConfigurationKeyFromFilename = function(test) {
+  test.expect(1);
+  test.equal(fileHandler('file.js').key(), 'file');
+  test.done();
+};
+
+exports.testRetrieveConfigurationKeyFromFilePath = function(test) {
+  test.expect(1);
+  test.equal(fileHandler('/some/path/file.js').key(), 'file');
+  test.done();
+};
